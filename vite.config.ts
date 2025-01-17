7c45d6fa1db166997c59af8d8be85a2d26d28435
Mradroit611 <sanjayvermaa12121@gmail.com>
2025-01-17 16:58:07 +0530

    solve error of vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,4 @@ export default defineConfig({
       '@components': '/src/components',
     },
   },
-  test: {
-    globals: true,  // This ensures that global functions like `expect` are available
-    environment: 'jsdom',  // Use jsdom for simulating a browser environment
-    setupFiles: './src/setupTests.ts',  // Specify setup file for jest-dom
-    css: true,  // If you're testing components with CSS
-  },
 });
